612d7a188b47941ea6eba87a6db4ed427d730ffd
apinanyogaratnam <apinan@ontopical.com>
2023-07-15 10:01:33 -0400

    chore: tidying code

--- driver/utils.go
+++ driver/utils.go
@@ -1,57 +1,55 @@
-package driver
+// package driver
 
-import (
-	"log"
-	"os"
+// import (
+// 	"log"
+// 	"os"
+// )
 
-	"google.golang.org/protobuf/proto"
-)
+// // TODO: update all this
+// func CreateBinDirIfNotExist() (error) {
+// 	if _, err := os.Stat("bin"); os.IsNotExist(err) {
+// 		if err != nil {
+// 			return err
+// 		}
 
-// TODO: update all this
-func CreateBinDirIfNotExist() (error) {
-	if _, err := os.Stat("bin"); os.IsNotExist(err) {
-		if err != nil {
-			return err
-		}
+// 		if err := os.Mkdir("bin", 0755); err != nil {
+// 			return err
+// 		}
+// 	}
 
-		if err := os.Mkdir("bin", 0755); err != nil {
-			return err
-		}
-	}
+// 	return nil
+// }
 
-	return nil
-}
+// func SaveObject(resource string, T proto.Message) (error) {
+// 	out, err := proto.Marshal(T)
+// 	if err != nil {
+// 		return err
+// 	}
 
-func SaveObject(resource string, T proto.Message) (error) {
-	out, err := proto.Marshal(T)
-	if err != nil {
-		return err
-	}
+// 	if err := CreateBinDirIfNotExist(); err != nil {
+// 		return err
+// 	}
 
-	if err := CreateBinDirIfNotExist(); err != nil {
-		return err
-	}
+// 	filename := "bin/" + resource + ".bin"
+// 	if err := os.WriteFile(filename, out, 0644); err != nil {
+// 		return err
+// 	}
 
-	filename := "bin/" + resource + ".bin"
-	if err := os.WriteFile(filename, out, 0644); err != nil {
-		return err
-	}
+// 	return nil
+// }
 
-	return nil
-}
+// func GetObject(resource string, data proto.Message) error {
+// 	filename := "bin/" + resource + ".bin"
 
-func GetObject(resource string, data proto.Message) error {
-	filename := "bin/" + resource + ".bin"
+// 	in, err := os.ReadFile(filename)
+// 	if err != nil {
+// 		log.Fatalln("error reading file:", err)
+// 		return err
+// 	}
 
-	in, err := os.ReadFile(filename)
-	if err != nil {
-		log.Fatalln("error reading file:", err)
-		return err
-	}
+// 	if err := proto.Unmarshal(in, data); err != nil {
+// 		return err
+// 	}
 
-	if err := proto.Unmarshal(in, data); err != nil {
-		return err
-	}
-
-	return nil
-}
+// 	return nil
+// }
--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module github.com/tachyondb/tachyondb
 
 go 1.20
-
-require google.golang.org/protobuf v1.31.0
-
-require github.com/google/go-cmp v0.5.9 // indirect
